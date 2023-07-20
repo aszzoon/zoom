77e125b5b21e05222ec85549a04831629447766c
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2023-07-20 23:16:32 +0900

    feat: frontend setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
+import path from 'path';
+
 import express from "express";
 
 const app = express();
+const __dirname = path.resolve();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+app.use("/public", express.static(__dirname + "/src/public"));
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3001);
+const handleListen = () => console.log('Listening on http://localhost:3000');
+//console.log("hello");
+
+app.listen(3000, handleListen);
