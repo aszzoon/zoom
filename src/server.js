40f1bbadf25ca6dcd2fb2ee1cd63a4dc8c7f74fa
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2023-08-01 17:18:35 +0900

    feat: ws

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 import path from 'path';
-
+import http from "http";
+import { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
 const __dirname = path.resolve();
 
 app.set("view engine", "pug");
+
 app.set("views", __dirname + "/src/views");
 app.use("/public", express.static(__dirname + "/src/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
-//console.log("hello");
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+server.listen(3000, handleListen);
+
+//console.log("hello");
+//app.listen(3000, handleListen)  
